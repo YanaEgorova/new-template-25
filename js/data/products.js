240dee8a1b4ae7b11c0710ff7f91c92580160792
Yana <yanaegorova69.3@gmail.com>
2021-09-16 20:32:53 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,149 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Ice Pack Relief Band',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'Cold Pack For Headaches',
+                text: 'This cold pack is flexible when frozen, with a soft side for gentle cooling relief & a white side for maximum strength cold therapy. This band can wrap around your head to provide relief from headaches, migraines & sinus pain.',
+            },
+            {
+                title: 'Migraine Ice Pack',
+                text: 'Cold packs & ice packs help reduce swelling & provide temporary numbing for pain. Cold packs & ice packs can also provide tension headache relief, migraine relief & sinus headaches relief.',
+            },
+            {
+                title: 'Headache Mask',
+                text: 'Suffer from migraines or headaches and sick of taking headache medicine? Simply store your gel ice pack in the freezer and use as needed as an ice pack for eyes or migraine wrap relief band.',
+            },
+            {
+                title: 'Migraine Mask',
+                text: 'Suffering from headaches? Need a migraine stop? This ice mask and headache ice pack can be used across the eyes or on the forehead as a headache relief band. Perfect for tension headache relief or sinus headache relief too.',
+            },
+            {
+                title: 'Eye Ice Pack',
+                text: 'A gel face mask cold pack that provides relief of sinusitis and sinus headaches. Simply store your relief band in the freezer and take it out when you need headache relief from sinus headaches, migraines, or tension headaches.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Container for Hardware',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                title: 'Impact Resistant Material',
+                text: 'Constructed from durable polypropylene, these storage drawers are made to last, and difficult to damage or break. The strong, see-through clear plastic drawers feature finger grip drawer pulls for easy opening.',
+            },
+            {
+                title: 'Desktop Or Wall Mountable',
+                text: 'The storage organizer is portable and can be placed on any desk, table, or work surface. The sturdy plastic frame features keyholes on the back for convenient wall mounting in a closet, in a classroom, workshop, or shed.',
+            },
+            {
+                title: 'Multi-Purpose',
+                text: 'With 42 bins (6 large, 36 small), this cabinet is ideal for storing and organizing everything from fishing tackle, small business supplies, hardware like nuts, screws, and bolts, to crafting materials, beads, makeup, or jewelry.',
+            },
+            {
+                title: 'Versatile',
+                text: 'These organizers are perfect for any area of the house! Get one for your garage, office, kitchen, bathroom, or bedroom. It’s the perfect organizational solution to keep accessories, pieces, or parts neatly stored and tidy.',
+            },
+            {
+                title: 'Product Details',
+                text: 'Material: Polypropylene. Overall Dimensions: (H)17” x (W) 14.5” x (D) 5.25”; Large Drawer Dimensions: (D)5.25” x (W)4.25” x (H) 2.25”; Small Drawer Dimensions: (D)5.25” x (W)2.25” x (H)2.25”',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Hand Truck',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Manufactured in USA; Harper exclusive I-Beam Glass Filled Nylon Plastic Frame is 30% lighter than steel and designed for maximum strength.',
+            },
+            {
+                text: 'Plastic Frame is factory assembled and is chemical, corrosion and rust resistant for longer life.',
+            },
+            {
+                text: 'Converts in seconds from 2 wheel dolly to 4 wheel cart - 300 lb dolly and 400 lb cart capacity (Product images illustrates both uses, but only one cart is being sold).',
+            },
+            {
+                text: '8" wheels with solid rubber tread and 3" swivel casters.',
+            },
+            {
+                text: 'Three position telescoping handle, built-in frame glides for easy moving up or down stairs and curbs.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Outdoor Garden Arch',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                title: 'Beautiful Decor',
+                text: 'Iron art European line design, beautiful arch that can be decorated with vibrant flowers, tulle, balloons, foliage, lights or anything else.Put some decorative lights on the garden arch to light up your garden, create a beautiful entry into your garden.',
+            },
+            {
+                title: 'Climbing Plants',
+                text: 'This arch will become the focal point of your garden, and will be a great choice for roses or other climbing plants, make the structure a nice choice for climbing vines like ivy, clematis, wisteria, grape vines, flowers and more, allows them to grow naturally up towards the top.',
+            },
+            {
+                title: 'Widely Application',
+                text: 'Perfect for various uses, for indoor/outdoor use also suits for many events such as wedding, anniversary and different parties.It can be used as a photo background at a party or as an entrance to the garden and backyard.',
+            },
+            {
+                title: 'High Quality',
+                text: 'Made of highquality iron material, which makes it very sturdy, durable and water resistant, Sturdy tubular Iron frame construction for long time using.',
+            },
+            {
+                title: 'Easy Assemble',
+                text: 'Assembly is easy, come with assembly instructions and accessories package, simple and convenient installation.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Wireless Charger',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Charges through most lightweight plastic cases – up to 3mm.',
+            },
+            {
+                text: 'All-in-one wireless charging solution for iPhone + Apple watch + AirPods, wirelessly charge all three at once.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Booster Car Seat',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Duo zone head and shoulder side-impact protection with 10 positions.',
+            },
+            {
+                text: 'Ergo Boost contoured seat with double foam padding.',
+            },
+            {
+                text: '2 dishwasher-safe cupfolders are easy to remove and hide away to save space.',
+            },
+            {
+                text: 'Backrest reclines and is removable to create a backless booster for older children; Product Depth (Footprint): 17"',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png', './img/img6-3.png'],
+    },
+]
